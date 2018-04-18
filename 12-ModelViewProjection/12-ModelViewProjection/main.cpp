ccd85b1101b79a431b27ecbad490670298a62fee
liutingluhe <liutingluhe@163.com>
2018-04-18 22:53:19 +0800

    add chapter04 all demo
    08. MoveByMatrix
    09. Objects
    10. Otrhographic
    11. Perspective
    12. ModelVieProjection
    13. SphereWorld-Lv1
    14. SphereWorld-Lv2
    15. SphereWorld-Lv3
    16. SphereWorld-Lv4

---
+++ 12-ModelViewProjection/12-ModelViewProjection/main.cpp
@@ -0,0 +1,124 @@
+//
+//  main.cpp
+//  12-ModelViewProjection
+//
+//  Created by luhe liu on 2018/4/18.
+//  Copyright © 2018年 luhe liu. All rights reserved.
+//
+
+#include <GLTools.h>    // OpenGL toolkit
+#include <GLMatrixStack.h>
+#include <GLFrame.h>
+#include <GLFrustum.h>
+#include <GLGeometryTransform.h>
+#include <GLBatch.h>
+#include <StopWatch.h>
+#include <math.h>
+#include <glut/glut.h>
+
+
+// Global view frustum class
+GLFrustum           viewFrustum;
+
+// The shader manager
+GLShaderManager     shaderManager;
+
+// The torus
+GLTriangleBatch     torusBatch;
+
+
+// Set up the viewport and the projection matrix
+void ChangeSize(int w, int h)
+{
+    // Prevent a divide by zero
+    if(h == 0)
+        h = 1;
+    
+    // Set Viewport to window dimensions
+    glViewport(0, 0, w, h);
+    
+    viewFrustum.SetPerspective(35.0f, float(w)/float(h), 1.0f, 1000.0f);
+}
+
+
+// Called to draw scene
+void RenderScene(void)
+{
+    // Set up time based animation
+    static CStopWatch rotTimer;
+    float yRot = rotTimer.GetElapsedSeconds() * 60.0f;
+    
+    // Clear the window and the depth buffer
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    
+    // Matrix Variables
+    M3DMatrix44f mTranslate, mRotate, mModelview, mModelViewProjection;
+    
+    // Create a translation matrix to move the torus back and into sight
+    m3dTranslationMatrix44(mTranslate, 0.0f, 0.0f, -2.5f);
+    
+    // Create a rotation matrix based on the current value of yRot
+    m3dRotationMatrix44(mRotate, m3dDegToRad(yRot), 0.0f, 1.0f, 0.0f);
+    
+    // Add the rotation to the translation, store the result in mModelView
+    m3dMatrixMultiply44(mModelview, mTranslate, mRotate);
+    
+    // Add the modelview matrix to the projection matrix,
+    // the final matrix is the ModelViewProjection matrix.
+    m3dMatrixMultiply44(mModelViewProjection, viewFrustum.GetProjectionMatrix(),mModelview);
+    
+    // Pass this completed matrix to the shader, and render the torus
+    GLfloat vBlack[] = { 0.0f, 0.0f, 0.0f, 1.0f };
+    shaderManager.UseStockShader(GLT_SHADER_FLAT, mModelViewProjection, vBlack);
+    torusBatch.Draw();
+    
+    
+    // Swap buffers, and immediately refresh
+    glutSwapBuffers();
+    glutPostRedisplay();
+}
+
+// This function does any needed initialization on the rendering
+// context.
+void SetupRC()
+{
+    // Black background
+    glClearColor(0.8f, 0.8f, 0.8f, 1.0f );
+    
+    glEnable(GL_DEPTH_TEST);
+    
+    shaderManager.InitializeStockShaders();
+    
+    // This makes a torus
+    gltMakeTorus(torusBatch, 0.4f, 0.15f, 30, 30);
+    
+    
+    glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+}
+
+
+///////////////////////////////////////////////////////////////////////////////
+// Main entry point for GLUT based programs
+int main(int argc, char* argv[])
+{
+    gltSetWorkingDirectory(argv[0]);
+    
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA | GLUT_DEPTH | GLUT_STENCIL);
+    glutInitWindowSize(800, 600);
+    glutCreateWindow("ModelViewProjection Example");
+    glutReshapeFunc(ChangeSize);
+    glutDisplayFunc(RenderScene);
+    
+    
+    GLenum err = glewInit();
+    if (GLEW_OK != err) {
+        fprintf(stderr, "GLEW Error: %s\n", glewGetErrorString(err));
+        return 1;
+    }
+    
+    SetupRC();
+    
+    glutMainLoop();
+    return 0;
+}
---
+++ 14-SphereWorld-Lv2/14-SphereWorld-Lv2/main.cpp
@@ -0,0 +1,191 @@
+//
+//  main.cpp
+//  14-SphereWorld-Lv2
+//
+//  Created by luhe liu on 2018/4/18.
+//  Copyright © 2018年 luhe liu. All rights reserved.
+//
+
+#include <GLTools.h>
+#include <GLShaderManager.h>
+#include <GLFrustum.h>
+#include <GLBatch.h>
+#include <GLFrame.h>
+#include <GLMatrixStack.h>
+#include <GLGeometryTransform.h>
+#include <StopWatch.h>
+
+#include <math.h>
+#include <stdio.h>
+
+#ifdef __APPLE__
+#include <glut/glut.h>
+#else
+#define FREEGLUT_STATIC
+#include <GL/glut.h>
+#endif
+
+
+GLShaderManager        shaderManager;            // Shader Manager
+GLMatrixStack        modelViewMatrix;        // Modelview Matrix
+GLMatrixStack        projectionMatrix;        // Projection Matrix
+GLFrustum            viewFrustum;            // View Frustum
+GLGeometryTransform    transformPipeline;        // Geometry Transform Pipeline
+
+GLTriangleBatch        torusBatch;
+GLBatch                floorBatch;
+GLTriangleBatch     sphereBatch;
+GLFrame             cameraFrame;
+
+//////////////////////////////////////////////////////////////////
+// This function does any needed initialization on the rendering
+// context. 
+void SetupRC()
+{
+    // Initialze Shader Manager
+    shaderManager.InitializeStockShaders();
+    
+    glEnable(GL_DEPTH_TEST);
+    glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+    
+    glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+    
+    // This makes a torus
+    gltMakeTorus(torusBatch, 0.4f, 0.15f, 30, 30);
+    
+    // This make a sphere
+    gltMakeSphere(sphereBatch, 0.1f, 26, 13);
+    
+    floorBatch.Begin(GL_LINES, 324);
+    for(GLfloat x = -20.0; x <= 20.0f; x+= 0.5) {
+        floorBatch.Vertex3f(x, -0.55f, 20.0f);
+        floorBatch.Vertex3f(x, -0.55f, -20.0f);
+        
+        floorBatch.Vertex3f(20.0f, -0.55f, x);
+        floorBatch.Vertex3f(-20.0f, -0.55f, x);
+    }
+    floorBatch.End();    
+}
+
+
+///////////////////////////////////////////////////
+// Screen changes size or is initialized
+void ChangeSize(int nWidth, int nHeight)
+{
+    glViewport(0, 0, nWidth, nHeight);
+    
+    // Create the projection matrix, and load it on the projection matrix stack
+    viewFrustum.SetPerspective(35.0f, float(nWidth)/float(nHeight), 1.0f, 100.0f);
+    projectionMatrix.LoadMatrix(viewFrustum.GetProjectionMatrix());
+    
+    // Set the transformation pipeline to use the two matrix stacks 
+    transformPipeline.SetMatrixStacks(modelViewMatrix, projectionMatrix);
+}
+
+
+// Respond to arrow keys by moving the camera frame of reference
+void SpecialKeys(int key, int x, int y)
+{
+    float linear = 0.1f;
+    float angular = float(m3dDegToRad(5.0f));
+    
+    if(key == GLUT_KEY_UP)
+        cameraFrame.MoveForward(linear);
+    
+    if(key == GLUT_KEY_DOWN)
+        cameraFrame.MoveForward(-linear);
+    
+    if(key == GLUT_KEY_LEFT)
+        cameraFrame.RotateWorld(angular, 0.0f, 1.0f, 0.0f);
+    
+    if(key == GLUT_KEY_RIGHT)
+        cameraFrame.RotateWorld(-angular, 0.0f, 1.0f, 0.0f);        
+}
+
+// Called to draw scene
+void RenderScene(void)
+{
+    // Color values
+    static GLfloat vFloorColor[] = { 0.0f, 1.0f, 0.0f, 1.0f};
+    static GLfloat vTorusColor[] = { 1.0f, 0.0f, 0.0f, 1.0f };
+    static GLfloat vSphereColor[] = { 0.0f, 0.0f, 1.0f, 1.0f };
+    
+    // Time Based animation
+    static CStopWatch    rotTimer;
+    float yRot = rotTimer.GetElapsedSeconds() * 60.0f;
+    
+    // Clear the color and depth buffers
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    
+    
+    // Save the current modelview matrix (the identity matrix)
+    modelViewMatrix.PushMatrix();    
+    
+    M3DMatrix44f mCamera;
+    cameraFrame.GetCameraMatrix(mCamera);
+    modelViewMatrix.PushMatrix(mCamera);
+    
+    // Draw the ground
+    shaderManager.UseStockShader(GLT_SHADER_FLAT,
+                                 transformPipeline.GetModelViewProjectionMatrix(),
+                                 vFloorColor);    
+    floorBatch.Draw();
+    
+    // Draw the spinning Torus
+    modelViewMatrix.Translate(0.0f, 0.0f, -2.5f);
+    
+    // Save the Translation
+    modelViewMatrix.PushMatrix();
+    
+    // Apply a rotation and draw the torus
+    modelViewMatrix.Rotate(yRot, 0.0f, 1.0f, 0.0f);
+    shaderManager.UseStockShader(GLT_SHADER_FLAT, transformPipeline.GetModelViewProjectionMatrix(),
+                                 vTorusColor);
+    torusBatch.Draw();
+    modelViewMatrix.PopMatrix(); // "Erase" the Rotation from before
+    
+    // Apply another rotation, followed by a translation, then draw the sphere
+    modelViewMatrix.Rotate(yRot * -2.0f, 0.0f, 1.0f, 0.0f);
+    modelViewMatrix.Translate(0.8f, 0.0f, 0.0f);
+    shaderManager.UseStockShader(GLT_SHADER_FLAT, transformPipeline.GetModelViewProjectionMatrix(),
+                                 vSphereColor);
+    sphereBatch.Draw();
+    
+    // Restore the previous modleview matrix (the identity matrix)
+    modelViewMatrix.PopMatrix();
+    modelViewMatrix.PopMatrix();    
+    // Do the buffer Swap
+    glutSwapBuffers();
+    
+    // Tell GLUT to do it again
+    glutPostRedisplay();
+}
+
+
+
+
+int main(int argc, char* argv[])
+{
+    gltSetWorkingDirectory(argv[0]);
+    
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
+    glutInitWindowSize(800,600);
+    
+    glutCreateWindow("OpenGL SphereWorld");
+    
+    glutSpecialFunc(SpecialKeys);
+    glutReshapeFunc(ChangeSize);
+    glutDisplayFunc(RenderScene);
+    
+    GLenum err = glewInit();
+    if (GLEW_OK != err) {
+        fprintf(stderr, "GLEW Error: %s\n", glewGetErrorString(err));
+        return 1;
+    }
+    
+    
+    SetupRC();
+    glutMainLoop();    
+    return 0;
+}
